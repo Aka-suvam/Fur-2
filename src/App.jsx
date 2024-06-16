332e28baca474f2db44459f6a5e302ab3d9a0100
Aka-suvam <subhamrai100@gmail.com>
2024-06-16 06:58:23 +0530

    proj

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./Component/Header/Header.jsx";
+import Footer from "./Component/Footer/Footer.jsx";
+
+import Home from "./pages/Home.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import Shop from "./pages/Shop.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+import ShoppingCart from './pages/ShoppingCart.jsx';
+import ProductPage from "./pages/ProductPage.jsx";
+
+import { Provider } from "react-redux";
+import appStore from "./ulits/appStore.jsx";
+
+
+const App=()=>{
+
+    
+    return(
+        
+        <>
+        <ToastContainer className="react-roast"/>
+       
+        <Header/> 
+       
+        <Outlet/>
+        
+       
+       <Footer/>
+      
+       </>
+      
+    );
+};
+
+const appRoute=createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            },
+            {
+
+                path:"/shop",
+                element:<Shop/>
+            },
+            {
+                path:"/products/:productId",
+                element:<ProductPage/>
+            },
+            {
+                path:"/about",
+                element:<AboutUs/>
+            },
+            {
+                path:"/contact",
+                element:<ContactUs/>
+            },
+            {
+                path:"/Shopping-cart",
+                element:<ShoppingCart/>
+            }]
+
+        }
+    
+]);
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render( 
+<Provider store={appStore}>
+<RouterProvider router={appRoute} />
+</Provider>
+);
+
+
